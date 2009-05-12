de17f55aa030c3a3340303d6127d9e9cbcd27074
Wes Winham <wes@wes-worktop.(none)>
2009-05-12 10:46:37 -0400

    Added optional argument to views for using alternative templates and form. Added list_attachments view with an optional json response based on the ACCEPT header

--- attachments/views.py
+++ attachments/views.py
@@ -3,20 +3,23 @@ from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
+from django.core import serializers
 
 from attachments.models import Attachment
 from attachments.forms import AttachmentForm
 
 
 @login_required
-def new_attachment(request, content_type, object_id):
+def new_attachment(request, content_type, object_id,
+                   template_name='attachments/new_attachment.html',
+                   form_cls=AttachmentForm):
     object_type = get_object_or_404(ContentType, id = int(content_type))
     try:
         object = object_type.get_object_for_this_type(pk=int(object_id))
     except object_type.DoesNotExist:
         raise Http404
     if request.method == "POST":
-        attachment_form = AttachmentForm(request.POST, request.FILES)
+        attachment_form = form_cls(request.POST, request.FILES)
         if attachment_form.is_valid():
             attachment = attachment_form.save(commit=False)
             attachment.content_type = object_type
@@ -25,13 +28,13 @@ def new_attachment(request, content_type, object_id):
             attachment.save()
             return HttpResponseRedirect(object.get_absolute_url())
     else:
-        attachment_form = AttachmentForm()
-    
-    return render_to_response("attachments/new_attachment.html", {
+        attachment_form = form_cls()
+
+    return render_to_response(template_name, {
         "attachment_form": attachment_form,
         "object": object
     }, context_instance=RequestContext(request))
-    
+
 @login_required
 def delete_attachment(request, attachment_slug):
     attachment = get_object_or_404(Attachment, slug=attachment_slug)
@@ -43,5 +46,24 @@ def delete_attachment(request, attachment_slug):
     if request.method == "POST":
         attachment.delete()
     return HttpResponseRedirect(object.get_absolute_url())
+
+@login_required
+def list_attachments(request, content_type, object_id,
+                   template_name='attachments/list_attachments.html'):
+    object_type = get_object_or_404(ContentType, id = int(content_type))
+    try:
+        object = object_type.get_object_for_this_type(pk=int(object_id))
+    except object_type.DoesNotExist:
+        raise Http404
+
+    attachments = Attachment.objects.attachments_for_object(object)
+    if request.META.get('HTTP_ACCEPT', None) == 'application/javascript':
+            data = serializers.serialize('json', attachments)
+            return HttpResponse(data, mimetype='application/javascript')
+    else:
+        return render_to_response(template_name, {
+            'object_list': attachments,
+            'object': object
+        }, context_instance=RequestContext(request))
 
 
