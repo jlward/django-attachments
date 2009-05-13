4409763f99cf7a11b105524589853841090e0e2e
Wes Winham <wes@wes-worktop.(none)>
2009-05-13 17:49:36 -0400

    RESTified urls to conform to object/action instead of action/object. Added url path to list all of an object's attachments

--- attachments/urls.py
+++ attachments/urls.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^newattachment/(?P<content_type>\d+)/(?P<object_id>\d+)/$', 'attachments.views.new_attachment', name='attachment_new'),
-    url(r'^deleteattachment/(?P<attachment_slug>[-\w]+)$', 'attachments.views.delete_attachment', name='attachment_delete'),
+urlpatterns = patterns('attachment.views',
+    url(r'^(?P<content_type>\d+)/(?P<object_id>\d+)/$',
+        'list_attachments',
+        name='attachment_list'),
+    url(r'^(?P<content_type>\d+)/(?P<object_id>\d+)/new/$',
+        'new_attachment',
+        name='attachment_new'),
+    url(r'^(?P<attachment_slug>[-\w]+)/delete/$',
+        'views.delete_attachment',
+        name='attachment_delete'),
 )
