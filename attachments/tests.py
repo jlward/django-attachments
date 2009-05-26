54ed63442130414838f15eb0f8d4c4d31d14779a
Wes Winham <wes@wes-worktop.(none)>
2009-05-26 15:39:50 -0400

    Revert "Added unit tests for the deepcopy function"
    This reverts commit fd61ca0def4ef9785b5970f323b065d7546f9d5d.

--- attachments/tests.py
+++ attachments/tests.py
@@ -1,13 +1,3 @@
-from django.test import TestCase
-from django.test.client import Client
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.core.files import File
-
-from attachments.models import Attachment, TestModel
-
-import os
-
 """
 
 >>> from attachments.models import Attachment, TestModel
@@ -45,41 +35,6 @@ import os
 
 >>> Attachment.objects.attachments_for_object(tm)
 [<Attachment: Something Else>, <Attachment: Something>]
+
+
 """
-
-class TestAttachmentCopying(TestCase):
-    def setUp(self):
-        self.client = Client(REMOTE_ADDR='localhost')
-        self.bob = User(username="bob")
-        self.bob.save()
-
-        self.tm = TestModel(name="Test1")
-        self.tm.save()
-        self.tm2 = TestModel(name="Test2")
-        self.tm2.save()
-
-        TEST_DIR = os.path.join(os.path.dirname(__file__))
-        self.TEST_FILE1 = os.path.join(TEST_DIR, "models.py")
-        self.TEST_FILE2 = os.path.join(TEST_DIR, "views.py")
-
-    def testDeepCopying(self):
-        """
-        Test that doing a deep copy of a file actually attempt to create a
-        second version of a file.
-        """
-        att1 = Attachment.objects.create_for_object(
-            self.tm, file=self.TEST_FILE1, attached_by=self.bob,
-            title="Something", summary="Something")
-        f = File(open(self.TEST_FILE1, 'wb'))
-        att1.file.save('models.py', f)
-
-        att2 = att1.copy(self.tm2, deepcopy=True)
-
-        # Ensure the saved_copy uses its proper file path
-        attachments = Attachment.objects.attachments_for_object(self.tm2)
-        for attachment in attachments:
-            self.assertEqual(
-                attachment.file.name,
-                Attachment.get_attachment_dir(attachment,
-                                              attachment.file_name())
-            )
